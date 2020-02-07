7e4f9952afb7e72a3a2eadeedcbbe15e194fb936
Astro <m.rahm7n@gmail.com>
2020-02-07 21:29:58 +0200

    [Update Resources to x310]

--- tests/5GNR/ran_oai_emulab_tst2.py
+++ tests/5GNR/ran_oai_emulab_tst2.py
@@ -11,7 +11,7 @@ request = portal.context.makeRequestRSpec()
 
 #Node0 @gNB
 node0 = request.RawPC( "node0" )
-node0.hardware_type = "d430"
+node0.hardware_type = "x310"
 node0.component_manager_id = "urn:publicid:IDN+emulab.net+authority+cm"
 
 #Modify disk_image for node0:
@@ -25,7 +25,7 @@ bs0.size = "10GB"
 
 node1 = request.RawPC( "node1" )
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+PowderTeam:update-nr-image"
-node1.hardware_type = "d430"
+node1.hardware_type = "x310"
 node1.component_manager_id = "urn:publicid:IDN+emulab.net+authority+cm"
 bs1 = node1.Blockstore("bs1", "/mytempdata")
 bs1.size = "10GB"
